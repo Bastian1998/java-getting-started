a4d909665132c2149349012b0938b5848f07d8e7
Bastian1998 <38963060+Bastian1998@users.noreply.github.com>
2020-12-04 16:57:48 -0300

    Create AlgoPedidosTest.java

---
+++ src/test/java/poo/coders/AlgoPedidosTest.java
@@ -0,0 +1,11 @@
+package poo.coders;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AlgoPedidosTest {
+    @Test
+    void 01PruebaSebas() {
+        Assertions.assertTrue(true);
+    }
+}
